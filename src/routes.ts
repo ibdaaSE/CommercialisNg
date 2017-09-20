53838fcbaa18dc3ddff57cc2799238475d98be61
User <user@Raouf.local>
2017-09-20 10:20:15 +0100

    slash routes

--- src/routes.ts
+++ src/routes.ts
@@ -5,5 +5,5 @@ import { ClientFournisseurComponent } from "app/clientFournisseur/cf.component";
 export const appRoutes:Routes = [
     {path : 'stock', component: StockComponent},
     {path: 'clientFournisseur', component: ClientFournisseurComponent},
-    {path: '', redirectTo: 'stock', pathMatch:'full'}
+    {path: '', redirectTo: '/stock', pathMatch:'full'}
 ]
